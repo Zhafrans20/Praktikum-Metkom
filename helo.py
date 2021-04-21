d3e91e988d30e3ed812a2579a1d2d3c09e39a70d
Zhafrans20 <82942111+Zhafrans20@users.noreply.github.com>
2021-04-21 19:42:56 +0700

    Add files via upload

---
+++ helo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#/=====================================/
+#/                                     /
+#/        Program halo.py              /
+#/ by muhammadzhafrans@mail.ugm.ac.id  /
+#/                                     /
+#/=====================================/
+
+
+# In[ ]:
+
+
+
+
